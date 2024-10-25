0890d1e7b2630c14fefe77501977ba6ed0181084
Oscar Mike <mike.claure@gmail.com>
2024-10-25 11:37:20 -0400

    Clean data discovery notebook

--- custom_notebooks/discovery/notebooks/DataDiscovery.py
+++ custom_notebooks/discovery/notebooks/DataDiscovery.py
@@ -1,6 +1,8 @@
 # Databricks notebook source
+# Using Databricks Runtime 15.2 ML
 import sys
 
+import plotly.express as px
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -15,21 +17,12 @@ from readers import read_from_volume
 
 # COMMAND ----------
 
-import plotly.express as px
-
-# COMMAND ----------
-
 spark = DatabricksSession.builder.getOrCreate()
 
 # COMMAND ----------
 
 df = read_from_volume(spark, "/databricks-datasets/nyctaxi-with-zipcodes/subsampled", as_pandas=True)
 output_file = "nyc.parquet"
-#df.to_parquet(path=f"../../data/{output_file}")
-
-# COMMAND ----------
-
-type(df)
 
 # COMMAND ----------
 
@@ -42,22 +35,9 @@ profile.to_notebook_iframe()
 
 # COMMAND ----------
 
-sns.pairplot(df)
-
-# COMMAND ----------
-
 fig = px.scatter_matrix(df.drop(labels=['tpep_pickup_datetime', 'tpep_dropoff_datetime'], axis=1))
 fig.update_traces(diagonal_visible=False)
 fig.update_layout(
     width=1500,
     height=1500)
 fig
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ----
